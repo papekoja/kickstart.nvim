b7ed2433839bd9845d7741b26fca37de01afce50
William <william@LINGOTIONMBA02s-Laptop.local>
2025-04-24 10:25:58 +0200

    added undo-tree

--- init.lua
+++ init.lua
@@ -899,6 +899,7 @@ require('lazy').setup({
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'kickstart.remap',
+  require 'kickstart.plugins.undo-tree',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
