afd4008b30a9813449c21e85be83e742dae69321
William <william@LINGOTIONMBA02s-Laptop.local>
2025-06-13 11:35:47 +0200

    some changes

--- init.lua
+++ init.lua
@@ -808,12 +808,12 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'catppuccin/nvim',
-    name = 'catppuccin',
+    'Mofiqul/vscode.nvim',
+    name = 'vscode.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       -- Load the colorscheme here.
-      vim.cmd.colorscheme 'catppuccin-latte'
+      vim.cmd.colorscheme 'vscode'
     end,
   },
 
